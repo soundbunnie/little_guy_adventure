fce296f30f825707bdc94925dbb644446f218ff1
Aria <ariari.elk@gmail.com>
2025-05-17 15:49:51 -0500

    Tweak jump to make it better

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,12 +1,22 @@
 extends CharacterBody2D
 class_name PlayerController
 
-@export var run_top_speed:float = 350.0
+@export var jump_multiplier = -30.0
 @export var JUMP_VELOCITY:float = 10.0
 
-var run_acceleration = 30.0
-var run_deceleration = 30.0
-var jump_multiplier = -30.0
+@export var jump_height:float = 100.0 # Height in pixels
+@export var jump_time_to_peak:float = 0.5 # Time in seconds
+@export var jump_time_to_descent:float = 0.4
+@export var fast_fall_multiplier: float = 0.5
+
+# Magic Math I stole
+@export var jump_velocity:float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0 
+@export var jump_gravity:float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@export var fall_gravity:float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_descent)) *-1.0
+
+@export var run_top_speed:float = 350.0
+@export var run_acceleration:float = 30.0
+@export var run_deceleration:float = 40.0
 
 var direction = 0
 
@@ -17,7 +27,13 @@ func _physics_process(delta):
 		velocity.y += gravity * delta # Apply gravity when airborne
 		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY * jump_multiplier 
+		jump()
+		
+	if Input.is_action_just_released("jump"): # Fast Fall
+		velocity.y *= fast_fall_multiplier 
+		
+	if Input.is_action_pressed("crouch") and is_on_floor(): # Make player stop when crouching
+		velocity.x = 0
 	# Direction will be equal to 1 if moving right and -1 if moving left
 	direction = Input.get_axis("move_left", "move_right") 
 	if direction:
@@ -29,5 +45,10 @@ func _physics_process(delta):
 	else:
 		# When the player stops inputting a direction, they will move to a stop (0) by the deceleration value
 		velocity.x = move_toward(velocity.x, 0, run_deceleration)
-		
 	move_and_slide()
+
+func get_jump_gravity() -> float:
+	return jump_gravity if velocity.y < 0.0 else fall_gravity
+	
+func jump():
+	velocity.y = jump_velocity
